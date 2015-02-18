7f2f157a9eb3557027965c8c3f7bf09626625f99
SERCAN <sercanuzumcu@gmail.com>
2015-02-18 11:08:44 +0200

    Bir Sayı Tut Oyunu First Commit

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnVar1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnYok1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnVar2 = new System.Windows.Forms.Button();
+            this.btnYok2 = new System.Windows.Forms.Button();
+            this.btnYok3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnVar3 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1-15 ARASINDA BİR SAYI TUTUN";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "BAŞLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnVar1
+            // 
+            this.btnVar1.Location = new System.Drawing.Point(38, 121);
+            this.btnVar1.Name = "btnVar1";
+            this.btnVar1.Size = new System.Drawing.Size(75, 23);
+            this.btnVar1.TabIndex = 2;
+            this.btnVar1.Text = "VAR";
+            this.btnVar1.UseVisualStyleBackColor = true;
+            this.btnVar1.Click += new System.EventHandler(this.btnVar1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // btnYok1
+            // 
+            this.btnYok1.Location = new System.Drawing.Point(146, 121);
+            this.btnYok1.Name = "btnYok1";
+            this.btnYok1.Size = new System.Drawing.Size(75, 23);
+            this.btnYok1.TabIndex = 5;
+            this.btnYok1.Text = "YOK";
+            this.btnYok1.UseVisualStyleBackColor = true;
+            this.btnYok1.Click += new System.EventHandler(this.btnYok1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(81, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 9;
+            // 
+            // btnVar2
+            // 
+            this.btnVar2.Location = new System.Drawing.Point(38, 193);
+            this.btnVar2.Name = "btnVar2";
+            this.btnVar2.Size = new System.Drawing.Size(75, 23);
+            this.btnVar2.TabIndex = 8;
+            this.btnVar2.Text = "VAR";
+            this.btnVar2.UseVisualStyleBackColor = true;
+            this.btnVar2.Click += new System.EventHandler(this.btnVar2_Click);
+            // 
+            // btnYok2
+            // 
+            this.btnYok2.Location = new System.Drawing.Point(146, 193);
+            this.btnYok2.Name = "btnYok2";
+            this.btnYok2.Size = new System.Drawing.Size(75, 23);
+            this.btnYok2.TabIndex = 10;
+            this.btnYok2.Text = "YOK";
+            this.btnYok2.UseVisualStyleBackColor = true;
+            this.btnYok2.Click += new System.EventHandler(this.btnYok2_Click);
+            // 
+            // btnYok3
+            // 
+            this.btnYok3.Location = new System.Drawing.Point(146, 280);
+            this.btnYok3.Name = "btnYok3";
+            this.btnYok3.Size = new System.Drawing.Size(75, 23);
+            this.btnYok3.TabIndex = 15;
+            this.btnYok3.Text = "YOK";
+            this.btnYok3.UseVisualStyleBackColor = true;
+            this.btnYok3.Click += new System.EventHandler(this.btnYok3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(81, 255);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 14;
+            // 
+            // btnVar3
+            // 
+            this.btnVar3.Location = new System.Drawing.Point(38, 280);
+            this.btnVar3.Name = "btnVar3";
+            this.btnVar3.Size = new System.Drawing.Size(75, 23);
+            this.btnVar3.TabIndex = 13;
+            this.btnVar3.Text = "VAR";
+            this.btnVar3.UseVisualStyleBackColor = true;
+            this.btnVar3.Click += new System.EventHandler(this.btnVar3_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(48, 255);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 12;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(48, 242);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 11;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(46, 314);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 16;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(45, 327);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 17;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(146, 353);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "YOK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(38, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "VAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(57, 401);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 13);
+            this.label12.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 437);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btnYok3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnVar3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnYok2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnVar2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnYok1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnVar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnVar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnYok1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnVar2;
+        private System.Windows.Forms.Button btnYok2;
+        private System.Windows.Forms.Button btnYok3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnVar3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        int a;
+        int b;
+        int c;
+        int d;
+        int[] bir = { 1, 3, 5, 7, 9, 11, 13, 15 };
+        int[] iki = { 2, 3, 6, 7, 10, 11, 14, 15 };
+        int[] uc = { 4, 5, 6, 7, 12, 13, 14, 15 };
+        int[] dort = { 8, 9, 10, 11, 12, 13, 14, 15 };
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnVar1.Visible = false;
+            btnYok1.Visible = false;
+            btnVar2.Visible = false;
+            btnYok2.Visible = false;
+            btnVar3.Visible = false;
+            btnYok3.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            label2.Text = "Tuttuğun Sayı Burada Var Mı? ";
+            label3.Text = "1, 3, 5, 7, 9, 11, 13, 15";
+            btnYok1.Visible = true;
+            btnVar1.Visible = true;
+
+           // label4.Text = "Tuttuğun Sayı Burada Var Mı? ";
+           // label5.Text = "1, 3, 5, 7, 9, 11, 13, 15";
+
+           
+    
+        }
+
+        private void btnVar1_Click(object sender, EventArgs e)
+        {
+            a = 1;
+            label4.Text = "Tuttuğun Sayı Burada Var Mı? ";
+            label5.Text = "2, 3, 6, 7, 10, 11, 14, 15";
+            btnVar2.Visible = true;
+            btnYok2.Visible = true;
+        }
+
+        private void btnYok1_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Tuttuğun Sayı Burada Var Mı? ";
+            label5.Text = "2, 3, 6, 7, 10, 11, 14, 15";
+            btnVar2.Visible = true;
+            btnYok2.Visible = true;
+        }
+
+        private void btnVar2_Click(object sender, EventArgs e)
+        {
+            b = 2;
+            label8.Text = "Tuttuğun Sayı Burada Var Mı? ";
+            label9.Text = "4, 5, 6, 7, 12, 13, 14, 15";
+            btnVar3.Visible = true;
+            btnYok3.Visible = true;
+        }
+
+        private void btnYok3_Click(object sender, EventArgs e)
+        {
+            label10.Text = "Tuttuğun Sayı Burada Var Mı? ";
+            label11.Text = "8, 9, 10, 11, 12, 13, 14, 15";
+          
+        }
+
+        private void btnYok2_Click(object sender, EventArgs e)
+        {
+            label8.Text = "Tuttuğun Sayı Burada Var Mı? ";
+            label9.Text = "4, 5, 6, 7, 12, 13, 14, 15";
+            btnVar3.Visible = true;
+            btnYok3.Visible = true;
+         
+        }
+
+        private void btnVar3_Click(object sender, EventArgs e)
+        {
+            c = 4;
+            label10.Text = "Tuttuğun Sayı Burada Var Mı? ";
+            label11.Text = "8, 9, 10, 11, 12, 13, 14, 15";
+          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            d = 8;
+            label12.Text = "TUTTUĞUNUZ SAYI: " + (a + b + c + d).ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label12.Text = "TUTTUĞUNUZ SAYI: " + (a + b + c + d).ToString();
+
+        }
+    }
+}
